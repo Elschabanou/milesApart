f9ca517a84b1a84bc4747643c3ad6ed09233bfd6
Elschabanou <felix@schabana.de>
2024-06-07 13:04:48 +0200

    second commit

---
+++ netlify/functions/eventData.js
@@ -0,0 +1,28 @@
+const { MongoClient } = require('mongodb');
+
+exports.handler = async (event, context) => {
+    const uri = "mongodb+srv://user:1234@library.ljxjrqs.mongodb.net/?retryWrites=true&w=majority&appName=Library";
+
+    const client = new MongoClient(uri, { useUnifiedTopology: true });
+
+    try {
+        await client.connect();
+        const database = client.db("Events");
+        const collection = database.collection("events");
+
+        // Query the collection to retrieve JSON data
+        const jsonData = await collection.find({}).toArray();
+
+        return {
+            statusCode: 200,
+            body: JSON.stringify(jsonData)
+        };
+    } catch (error) {
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ error: error.message })
+        };
+    } finally {
+        await client.close();
+    }
+};
